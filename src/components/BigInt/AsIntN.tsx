91e581973a6eceb4193f0c6c31d0cdeb0bf936b1
xiraynedev <xiraynedev@gmail.com>
2022-03-23 20:15:25 -0400

    add AsIntN component

--- src/components/BigInt/AsIntN.tsx
+++ src/components/BigInt/AsIntN.tsx
@@ -2,9 +2,17 @@ import {FC} from 'react';
 
 export const AsIntN: FC = () => {
 
-  const prevMaxSafeInteger = 9007199254740991n;
+  const max = 2n ** (64n - 1n) - 1n;
 
-  console.log(typeof prevMaxSafeInteger);
+  const check64bit = (number: bigint) => {
+    number > max
+      ? console.log(`Number doesn't fit in signed 64-bit integer.`)
+      : console.log(BigInt.asIntN(64, number));
+  };
+
+  check64bit(2n ** 64n);
+  check64bit(2n ** 32n);
+
 
   return (
     <div>
