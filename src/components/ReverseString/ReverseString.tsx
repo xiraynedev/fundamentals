1d56a9537d419b3f4bb590eeea603c5a7b07564b
xiraynedev <xiraynedev@gmail.com>
2022-05-24 13:07:26 -0400

    finish implementing ReverseString

--- src/components/ReverseString/ReverseString.tsx
+++ src/components/ReverseString/ReverseString.tsx
@@ -4,17 +4,20 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 export const ReverseString: FC = () => {
-
-  const [wordString, setWordString] = useState('Go ahead and reverse this string.')
+  const [wordString, setWordString] = useState(
+    'Go ahead and reverse this string.',
+  );
 
   const handleClick = () => {
-    
-  }
+    setWordString(wordString.split('').reverse().join(''));
+  };
 
   return (
     <Stack spacing={2} margin={10} width={200}>
       <Typography>{wordString}</Typography>
-      <Button variant='contained' onClick={handleClick}>Reverse String</Button>
+      <Button variant='contained' onClick={handleClick}>
+        Reverse String
+      </Button>
     </Stack>
   );
 };
